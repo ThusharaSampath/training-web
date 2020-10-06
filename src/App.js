0bb3d3c1ac973a1ba539456c15a8827f5596ea33
ThusharaSampath <59133724+ThusharaSampath@users.noreply.github.com>
2020-10-06 11:15:36 +0530

    card done
    card done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
+import DiaryCard from "./components/DiaryCard/DiaryCard";
+
 
 function App() {
+  var dimensions = [
+    { width:800, height: 1200 },
+    { width: 800, height: 600 },
+    { width: 800, height: 800 },
+    { width: 800, height: 600 },
+    { width: 800, height: 1200 },
+    { width: 800, height: 600 },
+    { width: 800, height: 400 },
+    { width: 800, height: 400 },
+    { width: 800, height: 400 },
+    { width: 800, height: 800 },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello Demo</h1>
-      </header>
+      <header className="App-header">Header</header>
+      <div class="CardArea">
+      <Columns dimensions={dimensions}>
+        <DiaryCard
+          title="This is title"
+          subtitle="this is subtitle"
+          discription="this is discriptionn
+          . thid discription must be collapsed after it length is excede 100 characters , so lets try"
+        />
+        <DiaryCard
+          title="This is title"
+          subtitle="this is subtitle"
+          discription="this is discription"
+        />
+        <DiaryCard
+          title="This is title"
+          subtitle="this is subtitle"
+          discription="this is discription"
+        />
+        <DiaryCard
+          title="This is title"
+          subtitle="this is subtitle"
+          discription="this is discription"
+        />
+        <DiaryCard
+          title="This is title"
+          subtitle="this is subtitle"
+          discription="this is discription"
+        />
+        <DiaryCard
+          title="This is title"
+          subtitle="this is subtitle"
+          discription="this is discription"
+        />
+        </Columns>
+      </div>
     </div>
   );
 }
---
+++ src/components/DiaryCard/DiaryCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./DiaryCard.css";
+
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import CardHeader from "@material-ui/core/CardHeader";
+
+class DiaryCard extends React.Component {
+  state = {
+    shown: true,
+  };
+  short = this.props.discription.substring(0, 100)+' ...';
+  long =  this.props.discription
+  render() {
+    return (
+      <Card className="card" style={{ backgroundColor: "lightBlue" }}>
+        <CardHeader title={this.props.title} subheader={this.props.subtitle} />
+
+        <CardActionArea>
+          <CardContent>
+            <h6>{
+                        this.state.shown ? 
+                        this.short:
+                        this.long
+                     }</h6>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            size="small"
+            color="primary"
+            class="btn"
+            onClick={() => this.setState({ shown: !this.state.shown })}
+          >
+            Show more
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+export default DiaryCard;
