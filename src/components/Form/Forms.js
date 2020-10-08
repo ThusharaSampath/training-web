a1bb944750f5d7403465751f0b1f73b280aeed0b
ThusharaSampath <59133724+ThusharaSampath@users.noreply.github.com>
2020-10-09 00:40:25 +0530

    sanack bar done
    but there is an error " findDOMNode is deprecated in StrictMode"

--- src/components/Form/Forms.js
+++ src/components/Form/Forms.js
@@ -2,16 +2,32 @@ import React from "react";
 import "./Forms.css";
 import Fab from "@material-ui/core/Fab";
 import TextField from "@material-ui/core/TextField";
-
+import Grid from "@material-ui/core/Grid";
+import Snackbar from "@material-ui/core/Snackbar";
 
 class Forms extends React.Component {
-
   state = {
     title: "",
     discription: "",
     Color: "black",
     error: "",
     hide: true,
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    this.setState({
+      open: false,
+    });
   };
 
   handleChange = (e) => {
@@ -29,6 +45,7 @@ class Forms extends React.Component {
         hide: true,
       });
     } else {
+      this.handleOpen()
       if (this.state.title.length > 0) {
         console.log("Missing Discription");
       } else {
@@ -46,47 +63,68 @@ class Forms extends React.Component {
       hide: true,
     });
   };
+
   render() {
     return (
       <div className="container" onMouseLeave={this.collapse}>
         <form onSubmit={this.handleSubmit}>
-          <div className="input-field col s6">
-            <TextField
-              id="title"
-              label="Submit New"
-              value={this.state.title}
-              onChange={this.handleChange}
-              variant="outlined"
-              fullWidth
-              onFocus={this.expand}
-              onMouseOver={this.expand}
-              multiline
-            />
+          <div className="container">
+            <Grid container spacing={1}>
+              <Grid item xs>
+                <TextField
+                  id="title"
+                  label="Submit New"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                  variant="outlined"
+                  fullWidth
+                  onFocus={this.expand}
+                  onMouseOver={this.expand}
+                  multiline
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Fab
+                  variant="outlined"
+                  size="medium"
+                  color="primary"
+                  style={{ width: "80px" }}
+                  aria-label="Add"
+                  onClick={(this.handleSubmit)}
+                  hidden={this.state.hide}
+                >
+                  Submit
+                </Fab>
+              </Grid>
+            </Grid>
+            <br></br>
+            <Grid item xs={12}>
+              <TextField
+                id="discription"
+                label="Description"
+                value={this.state.discription}
+                onChange={this.handleChange}
+                variant="outlined"
+                fullWidth
+                multiline
+                hidden={this.state.hide}
+              />
+            </Grid>
           </div>
-
-          <div className="input-field col s6">
-            <TextField
-              id="discription"
-              label="Description"
-              value={this.state.discription}
-              onChange={this.handleChange}
-              variant="outlined"
-              fullWidth
-              multiline
-              hidden={this.state.hide}
-            />
-          </div>
-          <Fab
-            variant="outlined"
-            size="small"
-            color="primary"
-            aria-label="Add"
-            onClick={this.handleSubmit}
-            hidden={this.state.hide}
-          >
-            Submit
-          </Fab>
         </form>
+        <Snackbar
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+          severity="error"
+          open={this.state.open}
+          autoHideDuration={4000}
+          onClose={this.handleClose}
+          message="PLEASE FILL BOTH FIELD TO ADD A NEW DIARY"
+          colo
+        >
+      </Snackbar>
       </div>
     );
   }
